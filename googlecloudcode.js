787598b9979e19a26020b39edf0df8e2e138945a
Jackson Borneman <me@jaxb.net>
2022-08-28 14:05:33 -0400

    Updated Google JS

--- googlecloudcode.js
+++ googlecloudcode.js
@@ -1,9 +1,9 @@
 // Derived from https://github.com/wilda17/ESP8266-Google-Calendar-Arduino/blob/master/calendar.gscript
 
-function DoCalanderOutput() {
+function doGet() {
     var events = GetEvents();
     Logger.log(events);
-    return ContentService.createTextOutput(events);
+    return ContentService.createTextOutput(JSON.stringify(events)).setMimeType(ContentService.MimeType.JSON);
 }
 
 function GetEvents() {
@@ -11,8 +11,9 @@ function GetEvents() {
     var Now = new Date();
     var Later = new Date();
     Later.setSeconds(Now.getSeconds() + 1036800); // Go 2 weeks from now
-    var events = Cal.getEvents(Now, Later); // Get the events
+    var events = Cal.getEvents(Now, Later, {"max": 25}); // Get the events
     var outevents = new Array();
+    Logger.log(Now.getTime())
     events.forEach(element => {
         // Create Event with only the required fields
         calevent = {
