1b923e5a2418b340db7f77cc7cd03efb422df476
Hobbes4Pres <75288580+Hobbes4Pres@users.noreply.github.com>
2022-09-04 14:47:51 -0400

    Update googlecloudcode.js
    Added separate ON and OFF events for each calendar event

--- googlecloudcode.js
+++ googlecloudcode.js
@@ -12,18 +12,39 @@ function GetEvents() {
     var Later = new Date();
     Later.setSeconds(Now.getSeconds() + 1036800); // Go 2 weeks from now
     var events = Cal.getEvents(Now, Later, {"max": 25}); // Get the events
-    var outevents = new Array();
+    var outEvents = new Array();
+    var calEvent, relayName;
+    
     Logger.log(Now.getTime())
     events.forEach(element => {
-        // Create Event with only the required fields
-        calevent = {
-            "End": element.getEndTime().getTime(),
+        // get Relay of event
+        relayName = element.getDescription().toUpperCase();
+
+        // Default Relay to Gate if not specified
+        if (relayName=="") {
+           relayName = "GATE";
+        };
+        
+        // Create On event with only the required fields based upon calendar Start time, for 15 minutes
+        calEvent = {
             "Start": element.getStartTime().getTime(),
-            "Title": element.getTitle(),
-            "ID": element.getId()
+            "End": element.getStartTime().getTime() + (60*15),
+            "Title": relayName + " ON",
+            "ID": element.getId() + "ON"
         };
         // Add to output array
-        outevents.push(calevent);
+        outEvents.push(calEvent);
+        
+        // Create Off event with only the required fields based upon calendar Start time, for 15 minutes
+        calEvent = {
+            "Start": element.getEndTime().getTime(),
+            "End": element.getEndTime().getTime() + (60*15),
+            "Title": relayName + " OFF",
+            "ID": element.getId() + "OFF"
+        };
+        // Add to output array
+        outEvents.push(calEvent);
+        
     });
-    return outevents;
+    return outEvents;
 }
