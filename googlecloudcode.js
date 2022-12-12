9237f4879590ad93884c492065e05fcdca546eb5
Hobbes4Pres <75288580+Hobbes4Pres@users.noreply.github.com>
2022-12-11 22:27:18 -0500

    Update googlecloudcode.js
    Changed the date/time format to be a string and how math is done on the times to adjust forwards/backwards. Also adjusted windows for events so start events end 1 minute before their normal calendar entry. end events last for 15 minutes.

--- googlecloudcode.js
+++ googlecloudcode.js
@@ -6,18 +6,26 @@ function doGet() {
     return ContentService.createTextOutput(JSON.stringify(events)).setMimeType(ContentService.MimeType.JSON);
 }
 
+function formatReadableDate(nDateTime) {
+    var sFormattedDT, dtEvent;
+    evtDT = new Date(nDateTime)
+
+    sFormattedDT = evtDT.getFullYear() + "-" + String(evtDT.getMonth()+1).padStart(2, "0") + "-" + String(evtDT.getDate()).padStart(2,"0") + " " + String(evtDT.getHours()).padStart(2, "0") + ":" + String(evtDT.getMinutes()).padStart(2, "0") + ":00";
+    return sFormattedDT;
+}
+
 function GetEvents() {
     var Cal = CalendarApp.getCalendarsByName('WW-Gate')[0]; // Get the calendar
     var Now = new Date();
     var Later = new Date();
-    Later.setSeconds(Now.getSeconds() + 1036800); // Go 2 weeks from now
+    Later.setDate(Now.getDate() + 14); // Go 2 weeks from now
     var events = Cal.getEvents(Now, Later, {"max": 25}); // Get the events
     var outEvents = new Array();
-    var calEvent, relayName;
+    var calEvent, relayName, dtStart, dtEnd;
     
     Logger.log(Now.getTime())
     events.forEach(element => {
-        // get Relay of event
+        // get Relay of event and force all caps for comparisons
         relayName = element.getDescription().toUpperCase();
 
         // Default Relay to Gate if not specified
@@ -25,21 +33,28 @@ function GetEvents() {
            relayName = "GATE";
         };
         
-        // Create On event with only the required fields based upon calendar Start time, for 15 minutes
+        // Create On event with only the required fields based upon calendar Start time, valid for the entire duration
+        // less one minute to avoid overlap with the off event
+        dtStart = element.getStartTime();
+        dtEnd = element.getEndTime();
+        dtEnd.setMinutes(dtEnd.getMinutes() - 1);
         calEvent = {
-            "Start": element.getStartTime().getTime(),
-            "End": element.getStartTime().getTime() + (60*15),
-            "Title": relayName + " ON",
+            "Start": formatReadableDate(dtStart),
+            "End": formatReadableDate(dtEnd),
+            "Action": relayName + " ON",
             "ID": element.getId() + "ON"
         };
         // Add to output array
         outEvents.push(calEvent);
         
-        // Create Off event with only the required fields based upon calendar Start time, for 15 minutes
+        // Create Off event with only the required fields based upon calendar end time, for 15 minutes
+        dtStart = element.getEndTime();
+        dtEnd = element.getEndTime();
+        dtEnd.setMinutes(dtEnd.getMinutes() + 15);
         calEvent = {
-            "Start": element.getEndTime().getTime(),
-            "End": element.getEndTime().getTime() + (60*15),
-            "Title": relayName + " OFF",
+            "Start": formatReadableDate(dtStart),
+            "End": formatReadableDate(dtEnd),
+            "Action": relayName + " OFF",
             "ID": element.getId() + "OFF"
         };
         // Add to output array
