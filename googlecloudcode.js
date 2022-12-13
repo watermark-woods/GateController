36cd4f292f573d7a7642b0cde8de3afd83675460
Hobbes4Pres <75288580+Hobbes4Pres@users.noreply.github.com>
2022-12-12 19:36:58 -0500

    Update googlecloudcode.js
    Changed date functions to explicitly use UTC. Added calendar event titles to JSON for debugging. Added application logging for debugging.

--- googlecloudcode.js
+++ googlecloudcode.js
@@ -10,7 +10,7 @@ function formatReadableDate(nDateTime) {
     var sFormattedDT, dtEvent;
     evtDT = new Date(nDateTime)
 
-    sFormattedDT = evtDT.getFullYear() + "-" + String(evtDT.getMonth()+1).padStart(2, "0") + "-" + String(evtDT.getDate()).padStart(2,"0") + " " + String(evtDT.getHours()).padStart(2, "0") + ":" + String(evtDT.getMinutes()).padStart(2, "0") + ":00";
+    sFormattedDT = evtDT.getUTCFullYear() + "-" + String(evtDT.getUTCMonth()+1).padStart(2, "0") + "-" + String(evtDT.getUTCDate()).padStart(2,"0") + " " + String(evtDT.getUTCHours()).padStart(2, "0") + ":" + String(evtDT.getUTCMinutes()).padStart(2, "0") + ":00";
     return sFormattedDT;
 }
 
@@ -22,8 +22,11 @@ function GetEvents() {
     var events = Cal.getEvents(Now, Later, {"max": 25}); // Get the events
     var outEvents = new Array();
     var calEvent, relayName, dtStart, dtEnd;
-    
-    Logger.log(Now.getTime())
+
+    // print out validation we are using UTC time
+    Logger.log("Window is " + Now.getUTCFullYear() +"-"+ (Now.getUTCMonth()+1) +"-"+ Now.getUTCDate() +" "+ Now.getUTCHours() +":"+ Now.getUTCMinutes() 
+              + " through " + Later.getUTCFullYear() +"-"+ (Later.getUTCMonth()+1) +"-"+ Later.getUTCDate() +" "+ Later.getUTCHours() +":"+ Later.getUTCMinutes());
+
     events.forEach(element => {
         // get Relay of event and force all caps for comparisons
         relayName = element.getDescription().toUpperCase();
@@ -41,19 +44,21 @@ function GetEvents() {
         calEvent = {
             "Start": formatReadableDate(dtStart),
             "End": formatReadableDate(dtEnd),
+            "Title": element.getTitle(),
             "Action": relayName + " ON",
             "ID": element.getId() + "ON"
         };
         // Add to output array
         outEvents.push(calEvent);
         
-        // Create Off event with only the required fields based upon calendar end time, for 15 minutes
+        // Create Off event with only the required fields based upon calendar end time, for 15 minutes for cushion
         dtStart = element.getEndTime();
         dtEnd = element.getEndTime();
         dtEnd.setMinutes(dtEnd.getMinutes() + 15);
         calEvent = {
             "Start": formatReadableDate(dtStart),
             "End": formatReadableDate(dtEnd),
+            "Title": element.getTitle(),
             "Action": relayName + " OFF",
             "ID": element.getId() + "OFF"
         };
@@ -61,5 +66,12 @@ function GetEvents() {
         outEvents.push(calEvent);
         
     });
+
+    // print to application log what the result is, one event per line
+    Logger.log("Start | End | Title | Action | ID");
+    outEvents.forEach(element => {
+      Logger.log(element["Start"] + " | " + element["End"] + " | " + element["Title"] + " | " + element["Action"] + " | " + element["ID"]);
+    });
+
     return outEvents;
 }
